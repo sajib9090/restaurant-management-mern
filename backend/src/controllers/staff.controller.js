f9249883705bdcc878e47cfc44c172ed3f5f1c79
sajib9090 <nispap.devil.sajib@gmail.com>
2024-02-06 18:45:12 +0600

    staff add and delete route setup

--- backend/src/controllers/staff.controller.js
+++ backend/src/controllers/staff.controller.js
@@ -1,12 +1,63 @@
 import createError from "http-errors";
 import Staff from "../models/staff.model.js";
+import { validateId } from "../helper/validateId.js";
+import findDataById from "../services/findDataById.js";
 
-const handleCreateStaff = async(req, res, next) => {
-    try {
-        
-    } catch (error) {
-        next(error);
+const handleCreateStaff = async (req, res, next) => {
+  try {
+    const { name } = req.body;
+
+    if (!name) {
+      throw createError(400, "Name is required.");
     }
-}
 
-export {handleCreateStaff}
+    const nameValidation = name.trim().replace(/\s+/g, " ").toLowerCase();
+
+    if (/^[^a-zA-Z]/.test(nameValidation)) {
+      throw createError(400, "Invalid name format.");
+    }
+
+    const existingName = await Staff.findOne({
+      name: nameValidation,
+    });
+
+    if (existingName) {
+      throw createError(400, "Name already exist");
+    }
+
+    const newStaffName = new Staff({
+      name: nameValidation,
+    });
+
+    // save to database
+    const savedStaff = await newStaffName.save();
+
+    res.status(200).send({
+      success: true,
+      message: "Staff created successfully",
+      savedStaff,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const handleDeleteStaff = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    validateId(id);
+
+    const options = {};
+    const staff = await findDataById(id, Staff, options, next);
+
+    await Staff.findByIdAndDelete(id);
+    res.status(200).send({
+      success: true,
+      message: "Staff deleted successfully",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export { handleCreateStaff, handleDeleteStaff };
--- backend/src/routers/staff.router.js
+++ backend/src/routers/staff.router.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { isLoggedIn } from "../middlewares/auth.js";
-import { handleCreateStaff } from "../controllers/staff.controller.js";
+import {
+  handleCreateStaff,
+  handleDeleteStaff,
+} from "../controllers/staff.controller.js";
 
 const StaffRouter = express.Router();
 
-
-StaffRouter.post("/table/add-table", isLoggedIn, handleCreateStaff);
-
+StaffRouter.post("/staff/add-staff", handleCreateStaff);
+StaffRouter.delete("/staff/delete/:id", isLoggedIn, handleDeleteStaff);
 
 export default StaffRouter;
