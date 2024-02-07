187cc47f4e13cc0f5417284b7a35aadadcd15e7c
sajib9090 <nispap.devil.sajib@gmail.com>
2024-02-08 00:19:15 +0600

    void invoice schema done

---
+++ backend/src/models/voidInvoice.model.js
@@ -0,0 +1,73 @@
+import mongoose from "mongoose";
+const { Schema, model } = mongoose;
+
+const itemSchema = new Schema({
+  _id: {
+    type: String,
+    required: true,
+    ref: "Menu-Item",
+  },
+  item_name: {
+    type: String,
+    required: true,
+  },
+  item_price_per_unit: {
+    type: Number,
+    required: true,
+  },
+  item_quantity: {
+    type: Number,
+    required: true,
+  },
+  table_name: {
+    type: String,
+    required: true,
+  },
+  total_price: {
+    type: Number,
+    required: true,
+  },
+});
+
+const VoidInvoiceSchema = new Schema(
+  {
+    serial_of_void: {
+      type: Number,
+      unique: true,
+    },
+    sold_invoice_id: {
+      type: Schema.ObjectId,
+      required: true,
+      ref: "Sold-Invoice",
+    },
+    table_name: {
+      type: String,
+      required: true,
+      ref: "Table",
+    },
+    void_quantity: {
+      type: Number,
+      required: true,
+    },
+
+    item: [itemSchema],
+  },
+  { timestamps: true }
+);
+
+VoidInvoiceSchema.pre("save", async function (next) {
+  if (!this.isNew) {
+    return next();
+  }
+
+  try {
+    const invoiceCount = await this.model("Void-Invoice").countDocuments({});
+    this.serial_of_void = invoiceCount + 1;
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+const VoidInvoice = model("Void-Invoice", VoidInvoiceSchema);
+export default VoidInvoice;
